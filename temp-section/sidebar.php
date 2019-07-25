97314e2a8fb3f075d94417c1dbfeecf6931f9aa5
Barrytron <barry@doodlecreative.ie>
2019-07-25 17:32:21 +0100

    Jeremy twattage

--- temp-section/sidebar.php
+++ temp-section/sidebar.php
@@ -4,7 +4,7 @@
 			<div class="column is-full"> 
 				<div class="fb-page" data-href="https://www.facebook.com/SkyWindowCork/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/SkyWindowCork/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/SkyWindowCork/">Sky Windows</a></blockquote></div>
 			</div>
-			<div>Jeremy is the best</div>
+			<div>Jeremy is a twat</div>
 			<!-- ////////////////// --> 
 		</div>
 </section>
